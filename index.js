27157494b727005c168510aeb8b923d9de944c8d
+ <+>
2023-11-03 02:49:43 -0500

    KT DOM modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const main = document.getElementById("main");
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Kendall is the champion";
+document.body.appendChild(newHeader);
+// dont forget to appenedChild to see it in the DOM
